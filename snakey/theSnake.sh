a02d4b3142e3dfee51efd4080f5c47871fb27f23
Jess <jsscherzog@yahoo.com>
2017-03-31 16:52:48 -0400

    evrythng

---
+++ snakey/theSnake.sh
@@ -0,0 +1,286 @@
+# this file loops through 29 images of a snake. when it runs, it looks like it is moving.
+
+clear
+file1="/txt/0-1.txt"
+while IFS= read -r line
+do
+	echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+file1="txt/0-2.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+file1="txt/0-3.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+file1="txt/0-4.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-5.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-6.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-7.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-8.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-9.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-10.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-11.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-12.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-13.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-14.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-15.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-16.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-17.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-18.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-19.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-20.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-21.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-22.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-23.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-24.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-25.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-26.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-27.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-28.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
+clear
+file1="txt/0-29.txt"
+while IFS= read -r line
+do
+  echo "$line"
+done <"$file1"
+sleep 0.2
+
+clear
+
